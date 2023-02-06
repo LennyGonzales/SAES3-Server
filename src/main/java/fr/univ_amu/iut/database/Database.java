969f94d70100c8bc637d71957a0f62ab2aad9b78
LennyGonzales <lenny.gonzales@etu.univ-amu.fr>
2023-02-06 16:04:46 +0100

    Externalize the database credentials

--- src/main/java/fr/univ_amu/iut/database/Database.java
+++ src/main/java/fr/univ_amu/iut/database/Database.java
@@ -1,17 +1,19 @@
+
 package fr.univ_amu.iut.database;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 /**
  * Supports the connection with the database and allows to close it
  * @author LennyGonzales
  */
 public class Database {
-    private static final String URL = "jdbc:postgresql://peanut.db.elephantsql.com/lhnhqbrm";
-    private static final String LOGIN = "lhnhqbrm";
-    private static final String PASSWORD = "KWjwsNe0T5pXCtXHh5M3tXy_ppanNir4";
+    private static final String DATABASE_OLD = "OLD";
     private static Connection connection;
 
     /**
@@ -19,10 +21,28 @@ public class Database {
      */
     public Database() {
         try {
-            connection = DriverManager.getConnection(URL, LOGIN, PASSWORD);
+            initConnections();
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public void initConnections() throws SQLException {
+        Properties env = new Properties();
+        try {
+            env.load(new FileInputStream("databaseCredentials.env"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        initSingleConnection(env, DATABASE_OLD);
+    }
+
+    public void initSingleConnection(Properties env, String databaseName) throws SQLException {
+        connection = DriverManager.getConnection(
+                env.getProperty("DB_" + databaseName + "_URL"),
+                env.getProperty("DB_" + databaseName + "_LOGIN"),
+                env.getProperty("DB_" + databaseName + "_PASSWORD")
+        );
     }
 
     /**
